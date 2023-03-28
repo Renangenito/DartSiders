723694d3ffefc307d3f8ec5d1afbeacaf333ecd0
Renan Lima <renangenito@hotmail.com>
2023-03-28 16:27:08 -0300

    criando_a_barra_de_pesquisa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import BarraPesquisa from './Components/Layout/BarraPesquisa';
 import SideBar from './Components/Layout/SideBar';
 
 function App() {
   return (
     <div className="App">
       <SideBar />
+      <BarraPesquisa />
     </div>
   );
 }
