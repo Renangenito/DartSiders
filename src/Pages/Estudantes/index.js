b17635fe481b9cc5d6207e352d57785710fa8358
Renan Lima <renangenito@hotmail.com>
2023-04-10 17:16:49 -0300

    fazendo_a_requisicao_lista_estudantes

--- src/Pages/Estudantes/index.js
+++ src/Pages/Estudantes/index.js
@@ -1,8 +1,17 @@
 import ItemLista from '../../Components/ItemLista';
 import LayoutPadrao from '../../Components/LayoutPadrao';
 import styles from './Estudantes.module.css';
+import { useEffect, useState } from 'react'
 
 function Estudantes() {
+    const [estudantes, setEstudantes] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/estudantes')
+            .then(resposta => resposta.json())
+            .then(dados => setEstudantes(dados))
+    }, [])
+
     return (
         <LayoutPadrao>
             <div className={styles.estudantes}>
@@ -11,9 +20,7 @@ function Estudantes() {
                     <button className={styles.botaoAdicionar}>Adicionar novos estudantes</button>
                 </div>
                 <section className={styles.secaoLista}>
-                    <ItemLista nome="Júlia" email="julia@gmail.com" telefone="48 996582154" matricula="1234567305477760" admissao="08/12/2021"/>
-                    <ItemLista nome="Júlia" email="julia@gmail.com" telefone="48 996582154" matricula="1234567305477760" admissao="08/12/2021"/>
-                    <ItemLista nome="Júlia" email="julia@gmail.com" telefone="48 996582154" matricula="1234567305477760" admissao="08/12/2021"/>
+                    {estudantes.map(estudante => <ItemLista key={estudante.id} imagemPerfil={estudante.imagem} nome={estudante.nome} email={estudante.email} telefone={estudante.telefone} matricula={estudante.matricula} admissao={estudante.admissao} />)}
                 </section>
             </div>
         </LayoutPadrao>
